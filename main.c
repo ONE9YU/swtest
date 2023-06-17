4e62196ebc434b4f67202c0268b589cc73ef2801
won9kyu <wonkyu3849@naver.com>
2023-06-17 18:47:34 +0900

    Commit6

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 
+int add(int a, int b);
 
 int main()
 {
@@ -11,9 +12,12 @@ int main()
 
 
 	int a;
-
+	int sum;
 
 	scanf("%d", &a);
+
+	sum = add(a, 3);
+	printf("%d", sum);
 
 	printf("tqtqq");
 
@@ -22,4 +26,11 @@ int main()
 	printf("12313");
 
 	return 0;
+}
+int add(int a, int b) {
+	int sum;
+
+	sum = a + b;
+	
+	return sum;
 }
